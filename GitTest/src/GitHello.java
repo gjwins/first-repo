d7a576949cae8151cade5f3997480e5ddcdfed32
gjwins <gunaseelanv@gmail.com>
2016-07-18 17:43:01 -0400

    bug fix first change

--- GitTest/src/GitHello.java
+++ GitTest/src/GitHello.java
@@ -3,8 +3,8 @@ public class GitHello {
 
 
 		public static void main(String[] args) {
-			System.out.println("Git Hello");
-			for(int i=0;i<20;i++) {
+			System.out.println("Git Hello from bugfix branch");
+			for(int i=0;i<100;i++) {
 				System.out.println("testing the loop : " + i);
 			}
 		}
