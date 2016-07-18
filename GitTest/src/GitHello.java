07f6c98539d97e8ea6474d2f1341e7229e774723
gjwins <gunaseelanv@gmail.com>
2016-07-18 17:38:16 -0400

    loop param changed

--- GitTest/src/GitHello.java
+++ GitTest/src/GitHello.java
@@ -4,7 +4,7 @@ public class GitHello {
 
 		public static void main(String[] args) {
 			System.out.println("Git Hello");
-			for(int i=0;i<10;i++) {
+			for(int i=0;i<20;i++) {
 				System.out.println("testing the loop : " + i);
 			}
 		}
