12d16878d11f68479ee64c9815d7a6b889e813ca
gjwins <gunaseelanv@gmail.com>
2016-07-18 17:35:50 -0400

    init commit

---
+++ GitTest/src/GitHello.java
@@ -0,0 +1,12 @@
+
+public class GitHello {
+
+
+		public static void main(String[] args) {
+			System.out.println("Git Hello");
+			for(int i=0;i<10;i++) {
+				System.out.println("testing the loop : " + i);
+			}
+		}
+
+	}
